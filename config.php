4ca7b6f67969c9d773b21671cf798d48a18b9ae6
Slengm4n <99735480+Slengm4n@users.noreply.github.com>
2025-10-28 11:43:35 -0300

    Update

--- config.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-// --- CONFIGURAÇÕES DO BANCO DE DADOS --- //
-define('DB_HOST', '');      // Endereço do servidor do banco de dados
-define('DB_NAME', '');       // Nome do banco de dados
-define('DB_USER', '');           // Nome de usuário para acesso ao banco
-define('DB_PASS', '');               // Senha para acesso ao banco
-define('DB_CHARSET', 'utf8mb4');     // Charset da conexão
